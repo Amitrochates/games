5dab0e586b509e7abfec992ce051b18831174fed
shlok agrawal <shlok.agrawal@hotmail.com>
2024-06-28 03:59:32 +0530

    commit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,23 @@
-import { Setup } from "@/components/Setup";
 
-export default function (){
+import { PrismaClient } from "@prisma/client";
+import { SetupComponent } from "@/components/SetupComponent";
+
+export default async function (){
+
+  
+    const prisma = new PrismaClient()
+     const setups = await prisma.setup.findMany()
+        const menu = await prisma.menu.findMany()
+        
+    
+    console.log(setups)
+    
     return <div>
         <div className="grid grid-cols-4">
-        <Setup/>
-        <Setup/>
-        <Setup/>
-        <Setup/>
+        {setups.map(setup => (
+           <SetupComponent number={setup.screenNo} type={setup.systemType} menu={menu}/>
+           
+      ))}
         </div>
         
     </div>
