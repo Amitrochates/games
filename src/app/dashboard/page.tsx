e8ab322547da403eff5e9542e9217d7537073d90
shlok agrawal <shlok.agrawal@hotmail.com>
2024-06-28 01:33:06 +0530

    Setup Component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { Setup } from "@/components/Setup";
+
+export default function (){
+    return <div>
+        <div className="grid grid-cols-4">
+        <Setup/>
+        <Setup/>
+        <Setup/>
+        <Setup/>
+        </div>
+        
+    </div>
+}
---
+++ src/components/Cart.tsx
@@ -0,0 +1,40 @@
+export const Cart = () => {
+    return (
+       <div>
+            <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="click" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" type="button">Dropdown hover <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+            </svg>
+            </button>
+
+            <div id="dropdownHover"    className="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
+                <ul className="py-2 text-sm text-gray-700" aria-labelledby="dropdownHoverButton">
+                <li>
+                    <div>
+                            <div className="flex items-center justify-between md:order-3 md:justify-end">
+                                <div className="flex items-center">
+                                <div> chips </div>
+                                <button type="button" id="decrement-button-4" data-input-counter-decrement="counter-input-4" className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100">
+                                    <svg className="h-2.5 w-2.5 text-gray-900
+                                    " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16" />
+                                    </svg>
+                                </button>
+                                <input type="text" id="counter-input-4" data-input-counter className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0" placeholder="" value="1" required />
+                                <button type="button" id="increment-button-4" data-input-counter-increment="counter-input-4" className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 ">
+                                    <svg className="h-2.5 w-2.5 text-gray-900 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                                    </svg>
+                                </button>
+                                </div>
+                                <div className="text-end md:order-2 md:w-32">
+                                <p className="text-base font-bold text-gray-900">$699</p>
+                                </div>
+                            </div>
+                        </div>
+                </li>
+               
+                </ul>
+    </div>
+</div>
+    )
+}
