93e38768f72b457d8dfb0b203538eccaaabe91fb
Tanay Bhutada <tanaybhutada03@gmail.com>
2024-07-02 22:51:46 +0530

    shlok vscode not working commit

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -20,6 +20,44 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json({ message: "session created" });
 }
+
+
+//  //GET input : screenid (abhi screenNo le rha, need to change schema)
+//   //response: session json
+// export async function GET(req: NextRequest){
+//   const body = await req.json();
+//   const prisma = new PrismaClient;
+//   const session = await prisma.session.findFirst({
+//     where:{
+//       screenNo: body.screenNo
+//     }
+//   })
+//   return NextResponse.json(session);
+// }
+
+// // PUT - gets the session info of current session from a find Request, calculates the bill amount and updates the amount of the given session
+// // input: session Id
+// // output: updated session with bill amount 
+// export async function PUT(req: NextRequest){
+//   const body = await req.json();
+//   const prisma = new PrismaClient;
+//   const session = await prisma.session.findFirst({
+//     where:{
+//       id: body.id,
+//     }
+//   })
+//   const billamount = (session.endAt - session.startAt)*(session.setup.baseRate + body.controller*session.setup.addRate) + 
+//   const updateSession = await prisma.session.update({
+//     where:{
+//       id: body.id
+//     },
+//     data:{
+//       amount: billamount
+//     }
+//   })
+  
+//   return NextResponse.json(updateSession);
+// }
  
 
 
