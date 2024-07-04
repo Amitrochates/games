924ddfeadfb1759bbac50757a549d7c3308afc75
shlok agrawal <shlok.agrawal@hotmail.com>
2024-07-04 16:03:59 +0530

    UI changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 
 import { PrismaClient } from "@prisma/client";
 import { ActivityCard } from "@/components/ActivityCard";
+import { TopBar } from "@/components/TopBar";
 
 export default async function (){
 
@@ -11,13 +12,18 @@ export default async function (){
     const menu = await prisma.menu.findMany()
         
     
-    return <div className="h-full w-full min-h-screen " style={{ backgroundColor: '#000000' }}>
-        <div className="grid grid-cols-3 gap-6 " >
+    return <div className="bg-black h-screen">
+      <div className="h-screen w-full min-h-screen bg-black">
+      <div className="">
+        <TopBar/>
+      </div>
+        <div className="grid grid-cols-3 gap-6" >
         {setups.map((setup: { id:number; screenNo: number; systemType: string; }) => (
            <ActivityCard id={setup.id} number={setup.screenNo} type={setup.systemType} menu={menu}/>
            
       ))}
       
+        </div>
         </div>
         </div>
         
--- src/components/IconGameController.tsx
+++ src/components/IconGameController.tsx
@@ -34,7 +34,7 @@ export const IconGameController: React.FC<ControllerProps> = ({ controllerCount,
  
   if(controllerStates){
     return (
-      <div onClick={handleDeActivate} className={"text-green-600 hover:text-green-200 mx-2"}> 
+      <div onClick={handleDeActivate} className={"text-red-600  hover:text-red-200 mx-2"}> 
         <svg
           viewBox="0 0 512 512"
           fill="currentColor"
@@ -49,7 +49,7 @@ export const IconGameController: React.FC<ControllerProps> = ({ controllerCount,
     );}
     else{
     return (
-      <div onClick={handleActivate} className={" text-slate-300 hover:text-green-200 mx-2"}> 
+      <div onClick={handleActivate} className={" text-slate-300 hover:text-red-200 mx-2"}> 
         <svg
           viewBox="0 0 512 512"
           fill="currentColor"
