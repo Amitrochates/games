b5103ba63a0e58b10062a4ea6ebfd8286db81ec3
Tanay Bhutada <tanaybhutada03@gmail.com>
2024-07-08 00:48:53 +0530

    added resposiveness for the gamecontrollers.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,36 +1,26 @@
-
 import { PrismaClient } from "@prisma/client";
 import { ActivityCard } from "@/components/ActivityCard";
 import { TopBar } from "@/components/TopBar";
 
-export default async function (){
+export default async function () {
+  const prisma = new PrismaClient();
+  const setups = await prisma.setup.findMany();
+  const menu = await prisma.menu.findMany();
 
-  
-    const prisma = new PrismaClient()
-    const setups = await prisma.setup.findMany();
-      
-    const menu = await prisma.menu.findMany()
-        
-    
-    return (
-      <div className="bg-black min-h-screen w-screen">
-        <div className="min-h-screen w-full bg-black flex flex-col">
-          <div>
-            <TopBar />
-          </div>
-          <div className="flex-grow overflow-auto">
-            <div className="grid grid-cols-3 gap-6">
-              {setups.map((setup: { id: number; screenNo: number; systemType: string; }) => (
-                <ActivityCard key={setup.id} id={setup.id} number={setup.screenNo} type={setup.systemType} menu={menu} />
-              ))}
-            </div>
+  return (
+    <div className="bg-black min-h-screen w-screen">
+      <div className="min-h-screen w-full bg-black flex flex-col">
+        <div>
+          <TopBar />
+        </div>
+        <div className="flex-grow overflow-auto">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 gap-6">
+            {setups.map((setup: { id: number; screenNo: number; systemType: string; }) => (
+              <ActivityCard key={setup.id} id={setup.id} number={setup.screenNo} type={setup.systemType} menu={menu} />
+            ))}
           </div>
         </div>
       </div>
-    );
-    
-    
-    
-        
-   
+    </div>
+  );
 }
